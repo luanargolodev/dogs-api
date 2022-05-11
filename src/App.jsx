3eb02115eb49c4a9a49f65a29d974fee2ad26af1
Luan Argolo <ipsluan@icloud.com>
2022-05-11 10:57:06 -0300

    :sparkles: feat: realizar o fetch na API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getImage() {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const json = await response.json();
+      setData(json);
+      setLoading(false);
+    }
+
+    getImage();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className="loading">Carregando...</div>
+      ) : (
+        <div className="App">
+          <header>
+            <h1 className="title">Dogs!</h1>
+          </header>
+          <main>
+            <div className="card">
+              <img src={data.message} alt="Cachorro" />
+              <button className="button">Buscar outro cachorro</button>
+            </div>
+          </main>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default App;
